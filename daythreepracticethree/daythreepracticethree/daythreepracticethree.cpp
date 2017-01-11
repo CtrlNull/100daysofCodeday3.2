d9594c8e8655d59ddd21daea68ceff9b906666cd
ctrlnull <raphaelfonseca88@hotmail.com>
2017-01-10 22:53:49 -0800

    finished prduct.  checking c++ safeguards(which doesnt have many)

---
+++ daythreepracticethree/daythreepracticethree/daythreepracticethree.cpp
@@ -0,0 +1,28 @@
+// This program unsafely accesses an area of memory by writing
+// valuse beyond an array's boundary
+// Warning: If you compile and run this program, it could crash.
+
+#include <iostream>
+#include "stdafx.h"
+using namespace std;
+
+
+int main()
+{
+	const int SIZE = 3;			// Constant for the array size
+	int values[SIZE];			// An array of 3 integers
+	int count;					// Loop counter variable
+
+	// Attempt to store fice numvers in the three-element array
+	cout << "I will store 5 numbers in a 3-element array! \n";
+	for (count = 0; count < 5; count++)
+		values[count] = 100;
+
+	// If the program is still running, display the numbers.
+	cout << "If you see this message, it means the program\n";
+	cout << "has not crashed! Here are the numbers:\n";
+	for (count = 0; count < 5; count++)
+		cout << values[count] << endl;
+    return 0;
+}
+
